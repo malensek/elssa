67d054a38406eefcf40f69d75713d34a449b3a28
Matthew Malensek <matt@malensek.net>
2014-08-14 22:19:40 -0600

    Add package-info for the buffer test

---
+++ src/galileo/test/buffer/package-info.java
@@ -0,0 +1,35 @@
+/*
+Copyright (c) 2014, Colorado State University
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are
+disclaimed. In no event shall the copyright holder or contributors be liable for
+any direct, indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused and on
+any theory of liability, whether in contract, strict liability, or tort
+(including negligence or otherwise) arising in any way out of the use of this
+software, even if advised of the possibility of such damage.
+*/
+
+/**
+ * Tests the client-side send buffer, which can be modified with the
+ * galileo.net.MessageRouter.writeQueueSize system property.  This buffer
+ * prevents clients from trying to send so much data that they run out of
+ * memory.  Since the send operation won't block, an application can easily
+ * generate too much data before the network operations are complete.
+ *
+ * @author malensek
+ */
+package galileo.test.buffer;
