78d82409a6c3315369eb0a2f21a143f3b573716a
Matthew Malensek <matt@malensek.net>
2015-10-03 23:54:04 -0600

    Add basic message implementation

---
+++ src/main/java/io/elssa/nn/ElssaMessage.java
@@ -0,0 +1,24 @@
+package io.elssa.nn;
+
+public class ElssaMessage {
+
+    private byte[] payload;
+
+    /**
+     * Constructs an ElssaMessage from an array of bytes.
+     *
+     * @param payload message payload in the form of a byte array.
+     */
+    public ElssaMessage(byte[] payload) {
+        this.payload = payload;
+    }
+
+    /**
+     * Retrieves the payload for this ElssaMessage.
+     *
+     * @return the ElssaMessage payload
+     */
+    public byte[] getPayload() {
+        return payload;
+    }
+}
