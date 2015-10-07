b3703a6ef46619d862500be6aab1f113792c199b
Matthew Malensek <matt@malensek.net>
2015-10-07 13:30:59 -0600

    Add base router implementation

--- src/main/java/io/elssa/net/DecoderState.java
+++ src/main/java/io/elssa/net/DecoderState.java
@@ -5,4 +5,3 @@ public enum DecoderState {
     READ_CONTENT;
 }
 
-
---
+++ src/main/java/io/elssa/net/MessageRouterBase.java
@@ -0,0 +1,44 @@
+package io.elssa.net;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class MessageRouterBase {
+
+    private List<MessageListener> listeners
+        = Collections.synchronizedList(new ArrayList<>());
+
+    /**
+     * Adds a message listener (consumer) to this MessageRouter.  Listeners
+     * receive messages that are published by this MessageRouter.
+     *
+     * @param listener {@link MessageListener} that will consume messages
+     * published by this MessageRouter.
+     */
+    public void addListener(MessageListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(MessageListener listener) {
+        listeners.remove(listener);
+    }
+
+    public void onConnnect(NetworkEndpoint endpoint) {
+        for (MessageListener listener : listeners) {
+            listener.onConnect(endpoint);
+        }
+    }
+
+    public void onDisconnect(NetworkEndpoint endpoint) {
+        for (MessageListener listener : listeners) {
+            listener.onDisconnect(endpoint);
+        }
+    }
+
+    public void onMessage(ElssaMessage msg) {
+        for (MessageListener listener : listeners) {
+            listener.onMessage(msg);
+        }
+    }
+}
