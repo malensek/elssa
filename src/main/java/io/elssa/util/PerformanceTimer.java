f3d2cf16913c6b349b2b1410614afb4195770582
Matthew Malensek <matt@malensek.net>
2015-10-06 20:15:21 -0600

    Move performance timer to main package

--- src/main/java/io/elssa/util/PerformanceTimer.java
+++ src/main/java/io/elssa/util/PerformanceTimer.java
@@ -23,7 +23,7 @@ any theory of liability, whether in contract, strict liability, or tort
 software, even if advised of the possibility of such damage.
 */
 
-package galileo.test.util;
+package io.elssa.util;
 
 import java.util.ArrayList;
 import java.util.Collections;
